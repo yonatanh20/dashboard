d429010c82bc790922d7b9b9636f5d22e1178184
yonatanh20 <yonatan.harari@gmail.com>
2022-07-19 16:24:54 +0300

    new Buttons

---
+++ lib/widgets/indicator-widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Indicator extends StatelessWidget {
+  const Indicator({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+        child: Card(
+            color: Theme.of(context).primaryColorLight,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            elevation: 2.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Icon(
+                  Icons.thermostat_auto_sharp,
+                  size: 60.0,
+                  color: Theme.of(context).focusColor,
+                ),
+                Text(""),
+              ],
+            )));
+  }
+}
